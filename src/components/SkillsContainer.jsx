6bff69fe39be6c23021754bc3154fe0cd3696250
Kamogelo Khumalo <KamoKhumalo04@gmail.com>
2022-10-28 20:18:01 +0200

    Minor update

--- src/components/SkillsContainer.jsx
+++ src/components/SkillsContainer.jsx
@@ -4,8 +4,8 @@ import "../styles/SkillsContainer.css";
 const SkillsContainer = ({ skills }) => {
   return (
     <div className="skills-container">
-      {skills.map((skill) => (
-        <div>{skill}</div>
+      {skills.map((skill, idx) => (
+        <div key={Math.random() + idx}>{skill}</div>
       ))}
     </div>
   );
